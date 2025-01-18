ed3fb4c5e717309042ad3ffbb1fce2ce3f4742f8
Michael Hsu <michaelhsu16@gmail.com>
2025-01-18 00:28:24 -0800

    Can manually select csv file now w/ minor error checking

--- Create-NewUserAccount.ps1
+++ Create-NewUserAccount.ps1
@@ -1,43 +1,43 @@
-<#
-Resources:
-New-ADUser - https://learn.microsoft.com/en-us/powershell/module/activedirectory/new-aduser?view=windowsserver2025-ps
-
-#>
-
-$NewEmployeeInfo = Import-Csv -Path "D:\Coding Files\Create-NewUserAccount\users_truncated.csv"
-
-function testfunction {
-    foreach ($employee in $NewEmployeeInfo) {
-        $employee.email
-    }
-}
-
 function Create-NewUserAccount {
-    foreach($employee in $NewEmployeeInfo) {
+    
+    $DialogBox = New-Object -TypeName System.Windows.Forms.OpenFileDialog
+
+    # Sets the file dialog starting point at C:\
+    $DialogBox.InitialDirectory = 'C:\'
+
+    # Filters which files will be displayed in the file dialog
+    $DialogBox.filter = 'csv files (*.csv)|*.csv'
+
+    $ImportSuccess = $true
+
+    # ShowDialog() displays the file dialog and captures the result (OK, CANCEL). If "OK", that means user selected a file and pressed OK.
+    if($DialogBox.ShowDialog() -eq 'OK') {
+        $NewEmployeeInfo = Import-Csv -path $DialogBox.FileName
+    }
+    else {
+        $ImportSuccess = $false
+    }
+
+
+    if ($ImportSuccess) {
         try {
-            $Password = ConvertTo-SecureString $employee.password -AsPlainText -Force
-            $FullName = $employee.Firstname + " " + $employee.LastName
+            foreach($employee in $NewEmployeeInfo) {
+        
+                $Password = ConvertTo-SecureString $employee.password -AsPlainText -Force
+                $FullName = $employee.Firstname + " " + $employee.LastName
 
-            # These parameters will throw errors, read docs on why:
-            # -Manager $employee.manager
-            # -phonenumber $employee.phonenumber
-            # -office $employee.officelocation
-            New-AdUser -GivenName $employee.FirstName -Surname $employee.LastName -Name $FullName -DisplayName $FullName -SamAccountName $employee.Username -EmailAddress $employee.email -AccountPassword $Password -ChangePasswordAtLogon $true -Department $employee.Department -Title $employee.jobtitle
-            "Successfully created user " + $employee.username
+                # May need to change arguments depending on wording of the headings in the CSV file
+                # Try to make sure it lines up with the parameters given here: 
+                # https://learn.microsoft.com/en-us/powershell/module/activedirectory/new-aduser?view=windowsserver2025-ps
+                New-AdUser -GivenName $employee.FirstName -Surname $employee.LastName -Name $FullName -DisplayName $FullName -SamAccountName $employee.Username -EmailAddress $employee.email -AccountPassword $Password -ChangePasswordAtLogon $true -Department $employee.Department -Title $employee.jobtitle -Enabled $true
+                "Successfully created user " + $employee.username
+
+            }
+        } catch {
+            'Failed to create user ' + $employee.username
         }
-        catch {
-            "Failed to create an account for user $FullName"
-        }
-
     }
-    Get-LocalUser
-}
 
-
-function DeleteTestAccounts {
-    foreach ($person in $NewEmployeeInfo) {
-        Remove-LocalUser -Name $person.Username
-        "User " + $person.Username + " has been deleted"
-    }
+    # For testing purposes
     Get-LocalUser
 }
