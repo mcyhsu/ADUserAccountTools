1c0a0dfe29e292cc7537b6842a7c30f23d0c8ba7
Michael Hsu <michaelhsu16@gmail.com>
2025-01-24 01:17:03 -0800

    Fixed a bug where Enable-BulkADUser doesn't return the correct message when passed a username that doesn't exist

--- ADUserAccountTools.ps1
+++ ADUserAccountTools.ps1
@@ -98,8 +98,8 @@ function Disable-Users {
 
         try {
             # Determine next steps based on if 1) the user exists, and 2) if they are enabled/disabled
-            $UserExists = if(Get-LocalUser -Name $UserReference -ErrorAction SilentlyContinue) { $true } else { $false }
-            $IsEnabled = if($(Get-LocalUser -Name $UserReference -ErrorAction SilentlyContinue).Enabled -eq $true) { $true } else { $false }
+            $UserExists = if(Get-ADUser -Identity $UserReference -ErrorAction SilentlyContinue) { $true } else { $false }
+            $IsEnabled = if($(Get-ADUser -Identity $UserReference -ErrorAction SilentlyContinue).Enabled -eq $true) { $true } else { $false }
 
             # Checks if the user account exists and is enabled before disabling the account
             if($UserExists -and $IsEnabled) {
@@ -118,19 +118,18 @@ function Disable-Users {
             $DisableResults = $DisableResults + $DisableResult
         }
         catch {
-            $Status = ''
-
-            # If there was an error in the previous step, i.e. the user doesn't exist or the user lacks permission, then display this warning message
-            if($UserExists -eq $false) {
-                Write-Host "Failed to disable user $($UserReference). " -ForegroundColor Red -NoNewline
-                Write-Host 'The user account may not exist or you lack the permission to disable this account.' -ForegroundColor Yellow
-                $Status = "Disabled"
-            }
+            $Status = 'Disabled'
 
             # Specifying that the account exists but is disabled prevents both log messages ("Account doesn't exist", "Account is already disabled") from displaying at the same time.
             if($IsEnabled -eq $false -and $UserExists) {
                 Write-Host "User account $UserReference is already disabled." -ForegroundColor Green
                 $Status = 'Disabled'
+            }
+            # If there was an error in the previous steps, i.e. a user doesn't exist or the admin lacks permission, then display this warning message
+            else {
+                Write-Host "Failed to disable user $($UserReference). " -ForegroundColor Red -NoNewline
+                Write-Host 'The user account may not exist or you lack the permission to disable this account.' -ForegroundColor Yellow
+                $Status = "Disabled"
             }
 
             # User doesn't exist or failed to be disabled, store the result as an object
